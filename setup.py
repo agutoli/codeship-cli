98d9643bcc6d65b2e269a920e16dee638146ac8c
Bruno Agutoli <bruno.agutoli@gmail.com>
2018-05-17 23:38:54 -0300

    updated info setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     version='0.0.4',
     packages=['codeship_cli', 'codeship_cli.commands'],
     scripts=['bin/codeship'],
+    description = 'Codeship client to help find text, see configurations by terminal',
     license='Codeship cli to manipulate projects',
+    author = 'Bruno Agutoli',
+    author_email = 'bruno.agutoli@gmail.com',
+    url = 'https://github.com/agutoli/codeship-cli',
+    download_url = 'https://github.com/agutoli/codeship-cli/archive/master.zip',
+    keywords = ['codeship', 'ci', 'codedeploy'],
     install_requires=[
         'requests==2.18.4',
         'pyyaml==3.12',
